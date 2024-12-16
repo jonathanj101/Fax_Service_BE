8504963e8268eca1520912d9eda9849783b2217b
jonathanj101 <jonatjimen@gmail.com>
2024-12-15 19:08:25 -0500

    jwt auth,common,helpers, server responses, implemented

---
+++ api/utils/auth/jwt_auth.py
@@ -0,0 +1,69 @@
+import jwt
+import datetime
+
+def create_access_jwtoken(user_id):
+    print("api/utils/auth/jwt_auth.create_access_jwtoken")
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+        'iat': datetime.datetime.utcnow()
+    }
+    try:
+        token = jwt.encode(payload, 'secret_access', algorithm='HS256')
+    except jwt.exceptions.PyJWKError as error:
+        print(token)
+        print("api/helpers/auth/jwt_auth.create_access_jwtoken error -> ", error)
+        # return error
+    return token
+
+
+def decode_access_jwtoken(token):
+    print("api/helpers/auth/jwt_auth.decode_access_jwtoken")
+    try:
+        payload = jwt.decode(token, 'secret_access', algorithms="HS256")
+        return payload['user_id']
+    except KeyError as error:
+        print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
+        return False, error
+    except jwt.exceptions.DecodeError as error:
+        print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
+        return False, error
+    except jwt.exceptions.ExpiredSignatureError as error:
+        print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
+        return False, error
+
+
+def create_refresh_jwtoken(user_id):
+    print("api/helpers/auth/jwt_auth.create_refresh_jwtoken")
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+        'iat': datetime.datetime.utcnow()
+    }
+
+    try:
+
+        token = jwt.encode(payload, 'secret_refresh', algorithm='HS256')
+    except KeyError as error:
+        print('jwt_auth.create_refresh_jwToken() error -> ', error)
+        return False, error
+
+    return token
+
+
+def decode_refresh_jwtoken(token):
+    print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken")
+    try:
+        payload = jwt.decode(token, 'secret_access', algorithms="H5256")
+
+    except KeyError as error:
+        print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
+        return False, error
+    except jwt.exceptions.DecodeError as error:
+        print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
+        return False, error
+    except jwt.exceptions.ExpiredSignatureError as error:
+        print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
+        return False, error
+
+    return payload['user_id']
---
+++ api/utils/helpers/helpers.py
@@ -0,0 +1,86 @@
+# Import modules
+from django.core.mail import send_mail
+import random
+
+
+def generate_random_password(length=30):
+    # GENERATE RANDOM PASSWORD, IN CASE USER FORGOT PASSWORD
+    print("helpers/services/services/generate_random_password")
+    text = ""
+    random_number = random.random() * length
+    random_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+"
+    for char in range(length):
+        text += random_chars[random.randint(
+            0, len(random_chars)-1)]
+    return text
+
+
+def forgot_password_notify_user(subject, message, subject2, user_email, username):
+    # NOTIFY USER WHEN FORGOT PASSWORD VIA EMAIL
+    print("herlpers/services/services/forgot_password_notify_user()")
+
+    html_message = f"""
+    <h1>
+        Forgot Password
+    </h1>
+    <br/>
+    <p>
+    Dear {username},
+        We received a reset password request. Please click the link below to continue to reset password!
+    </p>
+    <h3>
+    </h3>
+    <br/>
+    <button>
+        <a href="link to reset password page on the client side">
+            reset password
+        </a>
+    </button>
+    """
+    # print(html_message)
+    mailed = send_mail(
+        subject,
+        message,
+        subject2,
+        [user_email],
+        False,
+        None,
+        None,
+        None,
+        html_message
+    )
+    if mailed:
+        return True
+    return False
+
+
+def reset_password_notify_user(subject, message, subject2, user_mail, username):
+    # NOTIFY USER WHEN RESET PASSWORD VIA EMAIL
+    print("helpers/services/services/reset_password_notify_user()")
+    html_message = f"""
+    <h1>
+        Password Reset Successfully!
+    </h1>
+    <br/>
+    <p>
+    Dear {username},
+        Your password have been reset successfully! Now, try to log in again with the new password you just set up!
+    </p>
+    <h3>
+    </h3>
+    <br/>
+    """
+    mailed = send_mail(
+        subject,
+        message,
+        subject2,
+        [user_mail],
+        False,
+        None,
+        None,
+        None,
+        html_message
+    )
+    if mailed:
+        return True,
+    return False
