5a2b797ef0b929303ea04ab923613bed0f296d1c
jonathanj101 <jonatjimen@gmail.com>
2024-12-21 00:30:44 -0500

    company task completed, model/views/urls tested and working and jwt as well

--- api/models/company_model.py
+++ api/models/company_model.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import DO_NOTHING
 from django.utils import timezone
+import uuid
 
 class CompanyModel(models.Model):
     business_name = models.CharField(max_length=100,null=False)
@@ -12,14 +13,11 @@ class CompanyModel(models.Model):
     business_contact_number = models.CharField(max_length=100,null=False)
     business_email = models.CharField(max_length=100,null=True)
     business_fax_number = models.CharField(max_length=100,null=False)
-    owner = models.ForeignKey("UserModel", on_delete=DO_NOTHING, null=False)
+    business_owner = models.ForeignKey("UserModel", on_delete=DO_NOTHING, null=False)
     business_type = models.CharField(max_length=100,null=False, default="retail")
     business_size = models.FloatField()
     timestamp = models.DateField(blank=True,default=timezone.now)
-    business_id = models.CharField(max_length=100, null=False)
-    
+    business_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     def __str__(self):
-        return f"('{self.business_name}', '{self.business_street_address}','{self.business_registered_country}','{self.business_registered_city}', '{self.business_registered_zipcode}', '{self.business_contact_number}', '{self.business_email}', '{self.business_fax_number}', {self.business_type}', {self.business_size}', {self.business_id}', {self.owner}', {self.timestamp}', )"
-    
-    
-    
+        return f"('{self.pk}', '{self.business_name}', '{self.business_street_address}','{self.business_registered_country}','{self.business_registered_city}', '{self.business_registered_zipcode}', '{self.business_contact_number}', '{self.business_email}', '{self.business_fax_number}', {self.business_type}', {self.business_size}', {self.business_id}', {self.business_owner}', {self.timestamp}', )"
--- api/module_urls/company_urls.py
+++ api/module_urls/company_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from api.views.company_views import get_company
+from api.views.company_views import get_company, register_company, update_company
 
 urlpatterns = [
-    path("get-company", get_company)
+    path("get-company-by-id/<str:id>", get_company),
+    path("register-company", register_company),
+    path("update-company/<str:id>", update_company),
 ]
