799e9f57bd441614abbd993456cd99062ac68f8f
jonathanj101 <jonatjimen@gmail.com>
2024-12-21 00:38:44 -0500

    company serializer created tested working

--- api/serializers/company_serializer.py
+++ api/serializers/company_serializer.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from api.models.company_model import CompanyModel
 
+
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = CompanyModel
-        fields = ["__all__"]
+        fields = [
+            "business_name",
+            "business_street_address",
+            "business_registered_country",
+            "business_registered_city",
+            "business_registered_zipcode",
+            "business_contact_number",
+            "business_email",
+            "business_fax_number",
+            "business_owner",
+            "business_type",
+            "business_size",
+            "business_id",
+        ]
