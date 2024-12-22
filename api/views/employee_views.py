0679fb6a9f29a9828dbcc39f758415b87a9fd59b
jonathanj101 <jonatjimen@gmail.com>
2024-12-22 02:13:08 -0500

    created employees views and tested, working as expected

---
+++ api/views/employee_views.py
@@ -0,0 +1,132 @@
+# contrib
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from dotenv import load_dotenv
+import uuid
+import json
+import logging
+import jwt
+from django.core.exceptions import FieldError
+
+# Modules
+from api.models.employee_model import EmployeeModel
+from api.serializers.employee_serializer import EmployeeSerializer
+from api.models.company_model import CompanyModel
+from api.models.user_model import UserModel
+
+# Helpers
+from api.utils.helpers.helpers import split_bearer_value
+from api.utils.server_responses.http_responses import (
+    SUCCESS,
+    SUCCESS_CODE,
+    SERVER_ERROR,
+    UN_AUTHORIZED,
+    FORBIDEN_ACCESS,
+)
+
+# Auth
+from api.utils.auth.jwt_auth import decode_access_jwtoken
+
+# Loading environment variables
+load_dotenv()
+
+
+@api_view(["POST"])
+def create_employee(request):
+    print("api.views.employee_views.create_employee()")
+
+    REQUEST_BODY = json.loads(request.body)
+
+    DATA = {"business_id": REQUEST_BODY["companyId"], "user_id": REQUEST_BODY["userId"]}
+    try:
+        token = split_bearer_value(request.headers["Authorization"])
+        isUser = decode_access_jwtoken(token)
+        try:
+            business = CompanyModel.objects.filter(
+                business_id=DATA["business_id"]
+            ).first()
+            user = UserModel.objects.filter(user_id=DATA["user_id"]).first()
+
+            EMPLOYEE = EmployeeModel(
+                company_id=business.business_id, user_id=user.user_id
+            )
+
+            serializer = EmployeeSerializer(
+                EmployeeModel.objects.filter(company_id=business.business_id), many=True
+            ).data[0]
+
+            EMPLOYEE.save()
+
+            return Response(
+                {
+                    "message": "SUCCESS",
+                    "data": serializer,
+                    "status": SUCCESS["STATUS"],
+                    "status_code": SUCCESS_CODE["STANDARD"],
+                }
+            )
+
+        except FieldError as error:
+            print("An exception occurred")
+            logging.error("An Invalid Request Occurred", error)
+            return Response(
+                {
+                    "message": SERVER_ERROR["MESSAGE"],
+                    "status": SERVER_ERROR["STATUS"],
+                    "status_code": SERVER_ERROR["CODE"],
+                }
+            )
+
+    except KeyError or TypeError as error:
+        logging.error("An Invalid Request Occurred", error)
+        return Response(
+            {
+                "message": UN_AUTHORIZED["MESSAGE"],
+                "status": UN_AUTHORIZED["STATUS"],
+                "status_code": UN_AUTHORIZED["CODE"],
+            }
+        )
+
+    except AttributeError or AssertionError as error:
+        logging.error("An Invalid Request Occurred", error)
+        return Response(
+            {
+                "message": SERVER_ERROR["MESSAGE"],
+                "status": SERVER_ERROR["STATUS"],
+                "status_code": SERVER_ERROR["CODE"],
+            },
+        )
+
+
+@api_view(["GET"])
+def get_employees_by_company_id(request, company_id):
+    print("api.views.employee_views.get_employee_by_company_id()")
+    try:
+        token = split_bearer_value(request.headers["Authorization"])
+        decode_token = decode_access_jwtoken(token)
+        EMPLOYEES = EmployeeModel.objects.filter(
+            company_id=uuid.UUID(company_id)
+        ).first()
+        serializer = EmployeeSerializer(
+            EmployeeModel.objects.filter(company_id=company_id), many=True
+        ).data
+
+        return Response(
+            {
+                "message": "SUCCESS",
+                "data": serializer,
+                "status": SUCCESS["STATUS"],
+                "status_code": SUCCESS_CODE["STANDARD"],
+            }
+        )
+        # to do - > need more errors handling
+    except KeyError or TypeError as error:
+        logging.error("An Error Occurred -> ", error)
+
+        return Response(
+            {
+                "message": UN_AUTHORIZED["MESSAGE"],
+                "status": UN_AUTHORIZED["STATUS"],
+                "status_code": UN_AUTHORIZED["CODE"],
+            }
+        )
