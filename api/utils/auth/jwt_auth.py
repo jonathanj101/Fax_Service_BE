427d846c132257ad91234bdd9e4b0a2ea4d97f28
jonathanj101 <jonatjimen@gmail.com>
2024-12-22 19:51:17 -0500

    updated and redo jwt auth codebase for readability and scalability purposes

--- api/utils/auth/jwt_auth.py
+++ api/utils/auth/jwt_auth.py
@@ -11,26 +11,26 @@ def create_access_jwtoken(user_id):
     try:
         token = jwt.encode(payload, 'secret_access', algorithm='HS256')
     except jwt.exceptions.PyJWKError as error:
-        print(token)
+        # print(token)
         print("api/helpers/auth/jwt_auth.create_access_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
-    return {"isToken": True, "token": token}
+        return {"isDecoded": False, "error": error}
+    return {"isDecoded": True, "payload": token}
 
 
 def decode_access_jwtoken(token):
     print("api/helpers/auth/jwt_auth.decode_access_jwtoken")
     try:
         payload = jwt.decode(token, 'secret_access', algorithms="HS256")
-        return {"isToken": True, "data": payload["user_id"]}
+        return {"isDecoded": True, "payload": payload["user_id"]}
     except KeyError as error:
         print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
     except jwt.exceptions.DecodeError as error:
         print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
     except jwt.exceptions.ExpiredSignatureError as error:
         print("api/helpers/auth/jwt_auth.decode_access_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
 
 
 def create_refresh_jwtoken(user_id):
@@ -46,9 +46,9 @@ def create_refresh_jwtoken(user_id):
         token = jwt.encode(payload, 'secret_refresh', algorithm='HS256')
     except KeyError as error:
         print('jwt_auth.create_refresh_jwToken() error -> ', error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
 
-    return {"isToken": True, "token": token}
+    return {"isDecoded": True, "payload": token}
 
 
 def decode_refresh_jwtoken(token):
@@ -57,12 +57,12 @@ def decode_refresh_jwtoken(token):
         payload = jwt.decode(token, "secret_access", algorithms="H5256")
     except KeyError as error:
         print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
     except jwt.exceptions.DecodeError as error:
         print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
     except jwt.exceptions.ExpiredSignatureError as error:
         print("api/helpers/auth/jwt_auth.decode_refresh_jwtoken error -> ", error)
-        return {"isToken": False, "error": error}
+        return {"isDecoded": False, "error": error}
 
-    return {"isToken": True, "data": +payload["user_id"]}
+    return {"isDecoded": True, "payload": payload["user_id"]}
