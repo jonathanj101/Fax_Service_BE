a9e51417e76cedb472e9f0f21076f0d73e7c623b
jonathanj101 <jonatjimen@gmail.com>
2024-12-22 23:49:46 -0500

    bug 'fixed', init file explicit imported employeemodel,not detecting file

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 from django.db.models.deletion import DO_NOTHING
 from django.utils import timezone
+import uuid
 
 class UserModel(models.Model):
     first_name = models.CharField(max_length=100,null=False)
@@ -12,9 +13,7 @@ class UserModel(models.Model):
     email = models.CharField(max_length=100,null=False,unique=True)
     timestamp = models.DateField(blank=True,default=timezone.now)
     role = models.CharField(max_length=100,null=False,default='user')
-    
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     def __str__(self):
-        return f"User (`{self.first_name}`, `{self.middle_name}`. `{self.last_name}`, `{self.email}`, `{self.username}`, `{self.role}` , `{self.timestamp}` )"
-    
-    
-    
+        return f"User (`{self.first_name}`, `{self.middle_name}`. `{self.last_name}`, `{self.email}`, `{self.username}`, `{self.role}` , `{self.user_id}` `{self.timestamp}` )"
