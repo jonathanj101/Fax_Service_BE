fe0c150683f664861b01cf57a2611692a17e1b08
jonathanj101 <jonatjimen@gmail.com>
2024-12-24 21:20:58 -0500

    urls for update employee

--- api/module_urls/employee_urls.py
+++ api/module_urls/employee_urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
-from api.views.employee_views import create_employee, get_employees_by_company_id
+from api.views.employee_views import (
+    create_employee,
+    get_employees_by_company_id,
+    update_employee_status,
+    # get_employee_by_company_id,
+)
 
 urlpatterns = [
     path("create-employee", create_employee),
     path("get-employees-by-company-id/<str:company_id>", get_employees_by_company_id),
+    path("update-employee/<str:employee_id>", update_employee_status),
+    # path("get-employee/<str:company_id>", get_employee_by_company_id),
 ]
