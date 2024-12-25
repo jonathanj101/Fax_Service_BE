a97def779b405df6f213f8c9dba820ac3aad5546
jonathanj101 <jonatjimen@gmail.com>
2024-12-24 19:43:41 -0500

    added unprocessible entity server response

--- api/utils/server_responses/http_responses.py
+++ api/utils/server_responses/http_responses.py
@@ -2,39 +2,27 @@ responses = {}
 
 # SUCCESS RESPONSES
 responses["SUCCESS"] = {
-    "CODE": {
-        "STANDARD": 200,
-        "CREATED": 201,
-        "ACCEPTED": 202,
-        "NOCONTENT": 204
-    },
-    "STATUS": True
+    "CODE": {"STANDARD": 200, "CREATED": 201, "ACCEPTED": 202, "NOCONTENT": 204},
+    "STATUS": True,
 }
 # UNPROCESSIBLE ENTITY
-# responses['UNPROCESSIBLE_ENTITY'] = {
-#     "CODE":422,
-
-# }
+responses["UNPROCESSIBLE_ENTITY"] = {
+    "CODE": 422,
+    "MESSAGE": "UNPROCESSABLE_ENTITY",
+    "STATUS": False,
+}
 
 # SERVER ERROR
-responses["SERVER_ERROR"] = {
-    "CODE": 500,
-    "MESSAGE": "DB_ERROR",
-    "STATUS": False
-}
+responses["SERVER_ERROR"] = {"CODE": 500, "MESSAGE": "DB_ERROR", "STATUS": False}
 
 # UNAUTHORIZED USER
-responses["UN_AUTHORIZED"] = {
-    "CODE": 401,
-    "MESSAGE": "NOT_AUTHORIZED",
-    "STATUS": False
-}
+responses["UN_AUTHORIZED"] = {"CODE": 401, "MESSAGE": "NOT_AUTHORIZED", "STATUS": False}
 
 # FORBIDEENACCESS
 responses["FORBIDENACCESS"] = {
     "CODE": 403,
     "MESSAGE": "FORBIDEN_ACCESS",
-    "STATUS": False
+    "STATUS": False,
 }
 
 SUCCESS = responses["SUCCESS"]
@@ -46,6 +34,7 @@ UN_AUTHORIZED = responses["UN_AUTHORIZED"]
 
 FORBIDEN_ACCESS = responses["FORBIDENACCESS"]
 
+UNPROCESSIBLE_ENTITY = responses["UNPROCESSIBLE_ENTITY"]
 
 # print(SUCCESS)
 # print(responses)
