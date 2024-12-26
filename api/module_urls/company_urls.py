33c26d2d224b54fa06e01d44a356414246a2dde2
jonathanj101 <jonatjimen@gmail.com>
2024-12-25 19:15:54 -0500

    imported middleware and each corresponding use case

--- api/module_urls/company_urls.py
+++ api/module_urls/company_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from api.views.company_views import get_company, register_company, update_company
+from api.middleware.auth import admin_auth_middleware
 
 urlpatterns = [
-    path("get-company-by-id/<str:id>", get_company),
-    path("register-company", register_company),
-    path("update-company/<str:id>", update_company),
+    path("get-company-by-id", admin_auth_middleware(get_company)),
+    path("register-compan", admin_auth_middleware(register_company)),
+    path("update-company", admin_auth_middleware(update_company)),
 ]
--- api/module_urls/user_urls.py
+++ api/module_urls/user_urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
+from api.middleware.auth import auth_middleware
 from api.views.user_views import login, log_out, register, get_logged_in_user_info, forgot_password,reset_password,update_user
 
 
 urlpatterns = [
-    path('login',login),
+    path("login", login),
     path("logout", log_out),
-    path('register', register),
-    path("get-logged-in-user-info", get_logged_in_user_info),
+    path("register", register),
+    path("get-logged-in-user-info", auth_middleware(get_logged_in_user_info)),
     path("forgot-password", forgot_password),
     path("reset-password", reset_password),
-    path("update-user", update_user),
-
+    path("update-user", auth_middleware(update_user)),
 ]
