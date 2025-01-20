92f0685867df0d76088d2b042ced79b823808e27
jonathanj101 <jonatjimen@gmail.com>
2025-01-19 22:31:25 -0500

    finance urls

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("company/", include("api.module_urls.company_urls")),
     path("employees/", include("api.module_urls.employee_urls")),
     path("files/", include("api.module_urls.gcs_urls")),
-    path("fax-services/", include("api.module_urls.fax_services_urls"))
+    path("fax-services/", include("api.module_urls.fax_services_urls")),
+    path("finance/", include("api.module_urls.finances_urls"))
 ]
